84032289f3c23d4217380594a6cf85e20b07180a
Tallaks <tallaksmain@gmail.com>
2022-10-18 04:19:19 +0400

    Тест на нахождение Project contex префаба в корне ресурсов, который проваливается

--- Siege-client/Assets/Scripts/Tests/Scenes/BootSceneTests.cs
+++ Siege-client/Assets/Scripts/Tests/Scenes/BootSceneTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using Zenject;
 
 namespace Kulinaria.Siege.Tests.Scenes
 {
@@ -20,6 +21,16 @@ namespace Kulinaria.Siege.Tests.Scenes
             // Assert
             Assert.IsTrue(asyncOperation.isDone);
             Assert.IsTrue(SceneManager.GetActiveScene().name == SceneNames.BootScene);
+        }
+
+        [Test]
+        public void WhenProjectContextResourceLoadingCalled_ThenProjectContextLoads()
+        {
+            // Arrange
+            // Act
+            var projectContextPrefab = Resources.Load<ProjectContext>("ProjectContext");
+            // Assert
+            Assert.IsNotNull(projectContextPrefab);
         }
     }
 }
