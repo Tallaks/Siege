0befabfcb5c79d6c3d243c8a87a75bede6df43b3
Tallaks <tallaksmain@gmail.com>
2022-11-03 04:03:10 +0400

    Тест на то, что тайлы являются взаимными соседями друг друга, проходится

--- Siege-client/Assets/Scripts/Tests/Tiles/NeighbourTilesTests.cs
+++ Siege-client/Assets/Scripts/Tests/Tiles/NeighbourTilesTests.cs
@@ -17,18 +17,18 @@ namespace Kulinaria.Siege.Tests.Tiles
 		{
 			var grid = new[,]
 			{
-				{ 1, 1, 1, 1, 0 }, 
+				{ 1, 1, 1, 1, 0 },
 				{ 0, 0, 0, 0, 1 },
 				{ 1, 1, 1, 1, 1 },
 				{ 0, 0, 1, 0, 1 },
 				{ 1, 0, 1, 1, 0 }
 			};
-			
+
 			PreInstall();
 
 			Container.BindFactory<CustomTile, TilemapFactory>().AsSingle();
 			Container.Bind<IGridMap>().To<Runtime.Gameplay.Battle.Prototype.GridMap>().FromNew().AsSingle();
-	
+
 			PostInstall();
 
 			_gridMap = Container.Resolve<IGridMap>();
@@ -37,15 +37,26 @@ namespace Kulinaria.Siege.Tests.Tiles
 		}
 
 		[UnityTest]
-		public IEnumerator WhenMapGenerated_ThenTilesHaveNoNeighbours()
+		public IEnumerator WhenMapGenerated_ThenTilesHaveRightNeighbourCount()
 		{
-			CustomTile tile0 = _gridMap.GetTile(0,0);
-			CustomTile tile1 = _gridMap.GetTile(2,0);
-			CustomTile tile2 = _gridMap.GetTile(1,2);
-			
+			CustomTile tile0 = _gridMap.GetTile(0, 0);
+			CustomTile tile1 = _gridMap.GetTile(2, 0);
+			CustomTile tile2 = _gridMap.GetTile(1, 2);
+
 			Assert.Zero(tile0.Neighbours.Count());
 			Assert.AreEqual(2, tile1.Neighbours.Count());
 			Assert.AreEqual(3, tile2.Neighbours.Count());
+			yield break;
+		}
+
+		[UnityTest]
+		public IEnumerator WhenMapGenerated_ThenTilesAreNeighboursToEachOther()
+		{
+			CustomTile tile0 = _gridMap.GetTile(0, 4);
+			CustomTile tile1 = _gridMap.GetTile(1, 4);
+
+			Assert.IsTrue(tile0.Neighbours.Contains(tile1));
+			Assert.IsTrue(tile1.Neighbours.Contains(tile0));
 			yield break;
 		}
 	}
