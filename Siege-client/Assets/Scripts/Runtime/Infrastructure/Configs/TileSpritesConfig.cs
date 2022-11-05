30fe4198702e88a58b267cc1f65db9e56d152271
Tallaks <tallaksmain@gmail.com>
2022-11-06 03:31:02 +0400

    Скриптабл обджект для спрайтов тайлов

---
+++ Siege-client/Assets/Scripts/Runtime/Infrastructure/Configs/TileSpritesConfig.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Kulinaria.Siege.Runtime.Infrastructure.Configs
+{
+	[CreateAssetMenu(fileName = "TileSpritesConfig", menuName = "Siege/TilesConfig", order = 10)]
+	public class TileSpritesConfig : ScriptableObject
+	{
+		public Sprite Tile8_0_0;
+		public Sprite Tile6_2_0;
+		public Sprite Tile0_4_4;
+		public Sprite Tile5_3_0;
+		public Sprite Tile7_1_0;
+		public Sprite Tile2_2_4;
+		public Sprite Tile4_2_2a;
+		public Sprite Tile4_2_2b;
+		public Sprite Tile3_3_2;
+		public Sprite Tile5_1_2;
+		public Sprite Tile2_3_3;
+		public Sprite Tile3_2_3;
+		public Sprite Tile4_4_0;
+		public Sprite Tile1_3_4;
+	}
+}
