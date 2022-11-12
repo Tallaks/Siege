0bb58796832c03d4f200af4d32fdcb2dde6ff4a5
Tallaks <tallaksmain@gmail.com>
2022-11-12 13:20:02 +0400

    Вращатель uv-шки для 7 соседей

--- Siege-client/Assets/Scripts/Runtime/Gameplay/Battle/Movement/Tiles/Rendering/SevenNeighboursPropertyChanger.cs
+++ Siege-client/Assets/Scripts/Runtime/Gameplay/Battle/Movement/Tiles/Rendering/SevenNeighboursPropertyChanger.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Kulinaria.Siege.Runtime.Extensions;
+using Kulinaria.Siege.Runtime.Gameplay.Battle.Movement.Tiles.Rendering.UvRotators;
 using Kulinaria.Siege.Runtime.Infrastructure.Configs;
 using UnityEngine;
 
@@ -9,59 +10,26 @@ namespace Kulinaria.Siege.Runtime.Gameplay.Battle.Movement.Tiles.Rendering
 	{
 		private readonly TileSpritesConfig _config;
 
-		public SevenNeighboursPropertyChanger(TileSpritesConfig config) => 
+		public SevenNeighboursPropertyChanger(TileSpritesConfig config) =>
 			_config = config;
 
 		public void ChangeMaterial(CustomTile sourceTile, Material material)
 		{
 			Vector2Int missingTilePos = sourceTile.OfMissingNeighboursPositions().First();
-
-			if (sourceTile[-1, -1] == missingTilePos)
+			if (missingTilePos.IsDiagonalPositionTo(sourceTile.CellPosition).HasValue)
 			{
-				material.SetTexture(TileRenderer.TileTex, _config.Tile7_1_0);
-				material.SetFloat(TileRenderer.AngleProperty, 90f);
-			}
-
-			if (sourceTile[-1, 1] == missingTilePos)
-			{
-				material.SetTexture(TileRenderer.TileTex, _config.Tile7_1_0);
-				material.SetFloat(TileRenderer.AngleProperty, 0f);
-			}
-
-			if (sourceTile[1, 1] == missingTilePos)
-			{
-				material.SetTexture(TileRenderer.TileTex, _config.Tile7_1_0);
-				material.SetFloat(TileRenderer.AngleProperty, 270f);
-			}
-
-			if (sourceTile[1, -1] == missingTilePos)
-			{
-				material.SetTexture(TileRenderer.TileTex, _config.Tile7_1_0);
-				material.SetFloat(TileRenderer.AngleProperty, 180f);
-			}
-
-			if (sourceTile[0, 1] == missingTilePos)
-			{
-				material.SetTexture(TileRenderer.TileTex, _config.Tile5_1_2);
-				material.SetFloat(TileRenderer.AngleProperty, 270f);
-			}
-
-			if (sourceTile[0, -1] == missingTilePos)
-			{
-				material.SetTexture(TileRenderer.TileTex, _config.Tile5_1_2);
-				material.SetFloat(TileRenderer.AngleProperty, 90f);
-			}
-
-			if (sourceTile[-1, 0] == missingTilePos)
-			{
-				material.SetTexture(TileRenderer.TileTex, _config.Tile5_1_2);
-				material.SetFloat(TileRenderer.AngleProperty, 0f);
-			}
-
-			if (sourceTile[1, 0] == missingTilePos)
-			{
-				material.SetTexture(TileRenderer.TileTex, _config.Tile5_1_2);
-				material.SetFloat(TileRenderer.AngleProperty, 180f);
+				if (missingTilePos.IsDiagonalPositionTo(sourceTile.CellPosition).Value)
+				{
+					material.SetTexture(TileRenderer.TileTex, _config.Tile7_1_0);
+					var rotator = new Uv7_1_0Rotator(missingTilePos);
+					material.SetFloat(TileRenderer.AngleProperty, rotator.AngleDeg(sourceTile));
+				}
+				else
+				{
+					material.SetTexture(TileRenderer.TileTex, _config.Tile5_1_2);
+					var rotator = new Uv5_1_2Rotator(missingTilePos);
+					material.SetFloat(TileRenderer.AngleProperty, rotator.AngleDeg(sourceTile));
+				}
 			}
 		}
 	}
