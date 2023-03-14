8ace7bb0a3a33a38841cfb0c85dc45433d1e318e
Tallaks <tallaksmain@gmail.com>
2023-03-15 02:07:12 +0400

    Спавн тайлов от хоста по конфигу

--- Siege-tools/BattleTrier/Assets/Scripts/Runtime/Gameplay/Maps/Map.cs
+++ Siege-tools/BattleTrier/Assets/Scripts/Runtime/Gameplay/Maps/Map.cs
@@ -11,6 +11,16 @@ namespace Kulinaria.Tools.BattleTrier.Runtime.Gameplay.Maps
     public void SpawnTiles(TileType[,] mapTiles)
     {
       Debug.Log("SpawnTiles");
+      for(var col = 0; col < mapTiles.GetLength(0); col++)
+      for (var row = 0; row < mapTiles.GetLength(1); row++)
+      {
+        if (mapTiles[col, row] == TileType.Default)
+        {
+          Tile tile = Instantiate(_tilePrefab, new Vector3(-7.5f, -4, 0) + new Vector3(col, row) * 0.7f, Quaternion.identity);
+          tile.NetworkObject.Spawn();
+          tile.NetworkObject.TrySetParent(NetworkObject);
+        }
+      }
     }
   }
 }
