1c179ae5ddead0b411bfb8b31e346f93c5e4cd0b
Tallaks <tallaksmain@gmail.com>
2023-04-02 00:05:08 +0400

    [#15] Переприсвоил ссылки на кнопку выхода с учетом последних изменений, удалил ненужный более префаб кнопки выхода

--- Siege-tools/BattleTrier/Assets/Scripts/Runtime/Network/Roles/UI/RoleMediator.cs
+++ Siege-tools/BattleTrier/Assets/Scripts/Runtime/Network/Roles/UI/RoleMediator.cs
@@ -1,7 +1,5 @@
-using Kulinaria.Tools.BattleTrier.Runtime.Infrastructure.Services.Applications;
 using Kulinaria.Tools.BattleTrier.Runtime.Network.Connection.States;
 using UnityEngine;
-using UnityEngine.UI;
 using Zenject;
 
 namespace Kulinaria.Tools.BattleTrier.Runtime.Network.Roles.UI
@@ -9,16 +7,11 @@ namespace Kulinaria.Tools.BattleTrier.Runtime.Network.Roles.UI
   public class RoleMediator : MonoBehaviour
   {
     [SerializeField] private RoleUi _roleUi;
-    [SerializeField] private Button _quitButton;
 
     [Inject] private IConnectionStateMachine _connectionStateMachine;
-    [Inject] private IApplicationService _applicationService;
 
-    public void Initialize()
-    {
+    public void Initialize() =>
       _roleUi.Initialize();
-      _quitButton.onClick.AddListener(_applicationService.QuitApplication);
-    }
 
     public void ConfigureUIForLobbyMode(RoleUiMode mode) => 
       _roleUi.ConfigureUiForLobbyMode(mode);
