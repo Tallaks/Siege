700a93d1495b60d9f1af4fc53d8aae5de8413b68
Tallaks <tallaksmain@gmail.com>
2023-04-09 21:54:24 +0400

    [#18] Убрал лишнее из геймплейного медиатора

--- Siege-tools/BattleTrier/Assets/Scripts/Runtime/Gameplay/UI/GameplayMediator.cs
+++ Siege-tools/BattleTrier/Assets/Scripts/Runtime/Gameplay/UI/GameplayMediator.cs
@@ -1,24 +1,15 @@
 using Kulinaria.Tools.BattleTrier.Runtime.Gameplay.Characters.Data;
 using Kulinaria.Tools.BattleTrier.Runtime.Gameplay.Characters.Ui;
 using Kulinaria.Tools.BattleTrier.Runtime.Gameplay.Maps.Selection.UI;
-using Kulinaria.Tools.BattleTrier.Runtime.Infrastructure.Services.Applications;
 using Kulinaria.Tools.BattleTrier.Runtime.Network.Roles;
 using UnityEngine;
-using UnityEngine.UI;
-using Zenject;
 
 namespace Kulinaria.Tools.BattleTrier.Runtime.Gameplay.UI
 {
   public class GameplayMediator : MonoBehaviour
   {
     [SerializeField] private MapSelectionUi _mapSelectionUi;
-    [SerializeField] private Button _quitButton;
     [SerializeField] private CharacterSelectionUi _characterSelectionUi;
-
-    [Inject] private IApplicationService _applicationService;
-
-    private void Awake() =>
-      _quitButton.onClick.AddListener(_applicationService.QuitApplication);
 
     public void InitializeMapSelectionUi(RoleState stateValue) =>
       _mapSelectionUi.Initialize(stateValue);
